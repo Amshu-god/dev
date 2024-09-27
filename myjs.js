8a9f6f4feedebaa694b14c52437d2315fee32cc8
Amshu-god <psglover35@gmail.com>
2024-09-27 17:40:53 +0545

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -1,1 +1,20 @@
 
+function validateForm() {
+  
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    // Replace these with your own logic to validate credentials
+    const validUsername = 'amshu god';
+    const validPassword = 'divan is gay';
+
+    if (username === validUsername && password === validPassword) {
+        window.location.href = 'home-la-la.html'; // Redirect to desired URL
+    } else {
+        errorMessage.textContent = 'Invalid username or password';
+        errorMessage.style.color = 'red';
+    }
+   
+}
